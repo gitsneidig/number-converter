59e24eca860d895b1160bea2e774ea16254872bf
gitsneidig <scott@scottneidig.com>
2021-05-27 21:55:14 -0500

    Add model used by core app code.

---
+++ NumberConverter/Models/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NumberConverter.Models
+{
+    public class Number
+    {
+        [Display(Name = "Number to Convert:")]
+        public string NumberBeforeConversion { get; set; }
+        public string ConvertedDigits { get; set; }
+        public string ConvertedFraction { get; set; }
+    }
+}
